1c4abf56d7380cbc8cc619268588cafbcd70a99e
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-24 16:24:04 +0000

    Indentation

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ let total = 0
 document.getElementById('results').addEventListener('click', report)
 
 function report () {
-first = document.getElementById('box1').value
-second = document.getElementById('box2').value
-first = parseInt(first)
-second = parseInt(second)
-for (let i = 0; i < first; i++) {
+  first = document.getElementById('box1').value
+  second = document.getElementById('box2').value
+  first = parseInt(first)
+  second = parseInt(second)
+  for (let i = 0; i < first; i++) {
   total = total + second
-}
-document.getElementById('report').innerHTML = total
-total = 0
+  }
+  document.getElementById('report').innerHTML = total
+  total = 0
 }
