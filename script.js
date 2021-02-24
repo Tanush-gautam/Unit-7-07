f28e4740ce9971487455efb8f1c6bab778de2eb1
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-24 16:12:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let first = 0
+let second = 0
+let total = 0
+
+document.getElementById('results').addEventListener('click', report)
+
+function report () {
+first = document.getElementById('box1').value
+second = document.getElementById('box2').value
+first = parseInt(first)
+second = parseInt(second)
+for (let i = 0; i < first; i++) {
+  total = total + second
+}
+document.getElementById('report').innerHTML = total
+total = 0
+}
